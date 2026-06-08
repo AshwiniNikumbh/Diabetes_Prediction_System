d5ed461a1b02b6f0934c682eb642d32f65a459c5
Ashwini Nikumbh <148055139+AshwiniNikumbh@users.noreply.github.com>
2026-06-08 19:04:22 +0530

    Add files via upload

---
+++ Diabetes_predictor_main/diseasePred.py
@@ -0,0 +1,71 @@
+# impor necessary libraaries
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+# Loading model
+
+dia_model = pickle.load(open("./savedModels/Diabetes.sav",'rb'))
+
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('Diabetes Disease Prediction System!!! Developed by Devyani, Ashwini, Mansi, Khushbu',
+                          
+                          ['Diabetes Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
+    
+    
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    
+    with col2:
+        Age = st.text_input('Age of the Person')
+    
+    
+    # code for Prediction
+    diab_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = dia_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
+
+
+
